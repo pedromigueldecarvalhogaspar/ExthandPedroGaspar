1bbea02aa22f6aefba7ed714bcd752a4b9e66322
Pedro Gaspar <pedro.m.c.gaspar@outlook.com>
2022-08-05 17:57:31 +0100

    SecondRequest_Final

--- CA/Run.cs
+++ CA/Run.cs
@@ -21,6 +21,8 @@ namespace CA
 
             string stringUrl = "https://api-pub.bitfinex.com/v2/trades/tBTCEUR/hist?limit=1000";
 
+            // Used a cleaner way to get the request
+
             using (var client = new HttpClient())
             {
                 var endpoint = new Uri(stringUrl);
@@ -31,31 +33,31 @@ namespace CA
                 int timeCount = 0;
                 int maxPrice = 0;
                 int minPrice = 0;
-                
+
+                // I understod that there was a query in the API that helped us especify the start and end of the TimeStamp, as we want the recent hour I didnt use the query and made an handmaid endpoint so I could control it.
+
                 for (int i = 0; i < obj.Count; i++)
-                {
-                    
-                    
-                    int search = obj[i][3];
+                {                                    
+                    int compare = obj[i][3];
                     const int hour = 3600000;
 
                     timeCount = obj[0][1] - obj[i][1];
                     maxPrice = obj[0][3];
                     minPrice = obj[0][3];
 
-                    if (search > maxPrice)
+                    if (compare > maxPrice)
                     {
-                        maxPrice = search;
+                        maxPrice = compare;
                     }
 
-                    if (search < minPrice)
+                    if (compare < minPrice)
                     {
-                        minPrice = search;
+                        minPrice = compare;
                     }
 
                     if (timeCount > hour)
                     {
-                        Console.WriteLine($"In the last {timeCount} miliseconds:\nMin value:{minPrice};\nMax value:{maxPrice};\nAverage value:{(maxPrice + minPrice) / 2}");
+                        Console.WriteLine($"In the last {timeCount} miliseconds:\nMin value:{minPrice};\nMax value:{maxPrice};\nAverage value:{(maxPrice + minPrice) / 2}\n");
                         return;
                     }
                 }
